7589a355e217412ec57e62b2b1e7a803b04de78f
Sachin Sharma <sachin616789@gmail.com>
2026-04-01 14:01:08 +0530

    Added

--- server-side/server.js
+++ server-side/server.js
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors({origin:process.env.CLIENT_URL, credentials: true}));
+app.use(cors({origin: process.env.CLIENT_URL, credentials: true}));
 app.use(express.json());
 
 // Routes
@@ -21,7 +21,7 @@ app.use('/api/orders', require('./routes/orders'));
 
 
 // MongoDB Connection
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/ecommerce')
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://sachinVashishtdb_user:ShivShankar22@cluster0.3o4awij.mongodb.net/?appName=Cluster0')
 .then(() => console.log('MongoDB Connected'))
 .catch(err => console.log(err));
 
